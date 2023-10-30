827f215270d80e6c210c890c8fd74fed2045719e
root <root@LAPTOP-GHDP3GVF>
2023-10-30 19:02:36 +0300

    Validated gamma, created file for task 2, created first version of file with value iteration implementation

--- HW3/Маслов_practice3_3.py
+++ HW3/Маслов_practice3_3.py
@@ -1,12 +1,9 @@
 from Frozen_Lake import FrozenLakeEnv
 import numpy as np
 import time
-import wandb
 
-#-----Env initialization-----
 env = FrozenLakeEnv()
 
-#-----Functions-----
 def get_q_values(v_values, gamma):
     q_values = {}
     for state in env.get_all_states():
@@ -37,8 +34,12 @@ def policy_evaluation_step(v_values, policy, gamma):
     new_v_values = init_v_values()
     for state in env.get_all_states():
         new_v_values[state] = 0
+        max_q = 0
         for action in env.get_possible_actions(state):
-            new_v_values[state] += policy[state][action] * q_values[state][action]
+            current_q = q_values[state][action]
+            if current_q > max_q:
+                max_q = current_q
+        new_v_values[state] = max_q
     return new_v_values
 
 def policy_evaluation(policy, gamma, eval_iter_n):
@@ -62,61 +63,23 @@ def policy_improvement(q_values):
 		policy[state][argmax_action] = 1
 	return policy
 
-#-----Gamma validation-----
 iter_n = 20
 eval_iter_n = 20
-gamma_range = [0.999, 0.99999, 0.0001]
-env_entering_n = 1000
-action_n = 1000
+gamma = 0.9
 
-wandb.login()
-config = {
-    "task":"Gamma validation",
-    "description":"Try different gamma values with fixed number of iterations.",
-    "iter_n":iter_n,
-    "eval_iter_n":eval_iter_n,
-    "gamma_range":gamma_range,
-    "env_entering_n":1000,
-    "action_n":1000
-}
-run = wandb.init(project="ods_rl-frozen_lake", config=config, name="Run 3")
+policy = init_policy()
+for _ in range(iter_n):
+	q_values = policy_evaluation(policy, gamma, eval_iter_n)
+	policy = policy_improvement(q_values)
 
-def policy_training(iter_n, eval_iter_n, gamma):
-    policy = init_policy()
-    for _ in range(iter_n):
-        q_values = policy_evaluation(policy, gamma, eval_iter_n)
-        policy = policy_improvement(q_values)
-    return policy
-
-def policy_validation(policy, env_entering_n, action_n):
-    total_rewards = []
-    for _ in range(env_entering_n):
-        total_reward = 0
-        state = env.reset()
-        for _ in range(action_n):
-            action = np.random.choice(env.get_possible_actions(state), p=list(policy[state].values()))
-            state, reward, done, _ = env.step(action)
-            total_reward += reward
-            
-            if done:
-                break
-        total_rewards.append(total_reward)
-    return np.mean(total_rewards)
-
-wandb.define_metric("gamma")
-wandb.define_metric("Mean total reward", step_metric="gamma")
-
-max_reward = 0
-max_gamma = 0
-for gamma in np.arange(*gamma_range):
-    policy = policy_training(iter_n, eval_iter_n, gamma)
-    mean_total_reward = policy_validation(policy, env_entering_n, action_n)
-    if mean_total_reward > max_reward:
-         max_gamma = gamma
-         max_reward = mean_total_reward
-    log_dict = {
-        "gamma": gamma,
-        "Mean total reward": mean_total_reward,
-    }
-    wandb.log(log_dict)
-print("Max gamma: {0}".format(max_gamma))
+total_reward = 0
+state = env.reset()
+for _ in range(1000):
+	action = np.random.choice(env.get_possible_actions(state), p=list(policy[state].values()))
+	state, reward, done, _ = env.step(action)
+	total_reward += reward
+	env.render()
+	time.sleep(0.5)
+	if done:
+		break
+print(total_reward)
