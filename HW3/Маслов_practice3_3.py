5eace3288becab94165d2b872aa8207cb78270d4
root <root@LAPTOP-GHDP3GVF>
2023-10-30 20:40:28 +0300

    First experiments on value iteration

--- HW3/Маслов_practice3_3.py
+++ HW3/Маслов_practice3_3.py
@@ -1,9 +1,12 @@
 from Frozen_Lake import FrozenLakeEnv
 import numpy as np
 import time
+import wandb
 
+#-----Env initialization-----
 env = FrozenLakeEnv()
 
+#-----Functions-----
 def get_q_values(v_values, gamma):
     q_values = {}
     for state in env.get_all_states():
@@ -34,12 +37,8 @@ def policy_evaluation_step(v_values, policy, gamma):
     new_v_values = init_v_values()
     for state in env.get_all_states():
         new_v_values[state] = 0
-        max_q = 0
         for action in env.get_possible_actions(state):
-            current_q = q_values[state][action]
-            if current_q > max_q:
-                max_q = current_q
-        new_v_values[state] = max_q
+            new_v_values[state] += policy[state][action] * q_values[state][action]
     return new_v_values
 
 def policy_evaluation(policy, gamma, eval_iter_n):
@@ -63,23 +62,76 @@ def policy_improvement(q_values):
 		policy[state][argmax_action] = 1
 	return policy
 
+#-----Additional functions to compare iteration methods
+def value_iteration_step(v_values, gamma):
+    q_values = get_q_values(v_values, gamma)
+    new_v_values = init_v_values()
+    for state in env.get_all_states():
+        new_v_values[state] = 0
+        max_q = 0
+        for action in env.get_possible_actions(state):
+            current_q = q_values[state][action]
+            if current_q > max_q:
+                max_q = current_q
+        new_v_values[state] = max_q
+    return new_v_values
+
+def value_iteration(iter_n, gamma):
+    policy = init_policy()
+    v_values = init_v_values()
+    for _ in range(iter_n):
+        v_values = value_iteration_step(v_values, gamma)
+        q_values = get_q_values(v_values, gamma)
+        policy = policy_improvement(q_values)
+    return policy
+
+def policy_iteration(iter_n, eval_iter_n, gamma):
+    policy = init_policy()
+    for _ in range(iter_n):
+        q_values = policy_evaluation(policy, gamma, eval_iter_n)
+        policy = policy_improvement(q_values)
+    return policy
+     
+
+#-----Comparising policy iteration and value iteration-----
 iter_n = 20
 eval_iter_n = 20
-gamma = 0.9
+gamma = 0.99979
+env_entering_n = 1000
+action_n = 1000
 
-policy = init_policy()
-for _ in range(iter_n):
-	q_values = policy_evaluation(policy, gamma, eval_iter_n)
-	policy = policy_improvement(q_values)
+wandb.login()
+config = {
+    "task":"Comparising policy iteration and value iteration",
+    "description":"Try different gamma values with fixed number of iterations.",
+    "iter_n":iter_n,
+    "eval_iter_n":eval_iter_n,
+    "gamma":gamma,
+    "env_entering_n":1000,
+    "action_n":1000
+}
+run = wandb.init(project="ods_rl-frozen_lake", config=config, name="Run 6")
 
-total_reward = 0
-state = env.reset()
-for _ in range(1000):
-	action = np.random.choice(env.get_possible_actions(state), p=list(policy[state].values()))
-	state, reward, done, _ = env.step(action)
-	total_reward += reward
-	env.render()
-	time.sleep(0.5)
-	if done:
-		break
-print(total_reward)
+def policy_validation(policy, env_entering_n, action_n):
+    total_rewards = []
+    for _ in range(env_entering_n):
+        total_reward = 0
+        state = env.reset()
+        for _ in range(action_n):
+            action = np.random.choice(env.get_possible_actions(state), p=list(policy[state].values()))
+            state, reward, done, _ = env.step(action)
+            total_reward += reward
+            
+            if done:
+                break
+        total_rewards.append(total_reward)
+    return np.mean(total_rewards)
+
+value_iteration_policy = value_iteration(iter_n, gamma)
+value_mean_reward = policy_validation(value_iteration_policy, env_entering_n, action_n)
+
+policy_iteration_policy = policy_iteration(iter_n, eval_iter_n, gamma)
+policy_mean_reward = policy_validation(policy_iteration_policy, env_entering_n, action_n)
+
+print("Mean total reward for value iteration: {0}".format(value_mean_reward))
+print("Mean total reward for policy iteration: {0}".format(policy_mean_reward))
